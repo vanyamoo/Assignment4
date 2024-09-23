4b49125cf138c21f15b88e29fe7fbb71887bc1f9
vanya <vanya.moo@gmail.com>
2024-09-23 09:34:10 +0100

    Initial Commit

---
+++ Sept24A4/Sept24A4App.swift
@@ -0,0 +1,17 @@
+//
+//  Sept24A4App.swift
+//  Sept24A4
+//
+//  Created by Vanya Mutafchieva on 23/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Sept24A4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
